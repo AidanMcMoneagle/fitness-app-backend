c2a6bd3f839517d48efea1739f056c05091027a8
Aidan McMoneagle <asmcm7@Gmail.com>
2023-01-13 17:18:49 +0000

    get workouts route added

--- controllers/workouts-controllers.js
+++ controllers/workouts-controllers.js
@@ -2,7 +2,21 @@ const mongoose = require("mongoose");
 const Workout = require("../models/workout-model");
 
 // do we need to extract  userId from workout. NO.
-const getWorkoutsByUserId = async (req, res, next) => {};
+const getWorkoutsByUserId = async (req, res, next) => {
+  const { userId } = req.userData;
+  let foundWorkouts;
+  try {
+    foundWorkouts = await Workout.find({ creator: userId });
+  } catch (e) {
+    const error = new HttpError(
+      "Something went wrong please try again later",
+      500
+    );
+    return next(error);
+  }
+  // we want to respond with all the data including the id. This allows us to include in the url when we send a request to delete/update a workout.
+  res.json({ foundWorkouts }); // even if foundWorkouts is empty we still send back. Then we handle this on the frontEnd.
+};
 
 // in the post route need to extract data from req.body. Then need to use the new Workout to create a new workout object.
 //now find the user in the db to associate the workout with. If we do not find a user we throw an error.
