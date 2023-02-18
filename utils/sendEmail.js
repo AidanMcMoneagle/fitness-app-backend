c07ebd1751d1325ca9b853a90b823feeca5b6522
Aidan McMoneagle <asmcm7@Gmail.com>
2023-02-18 23:09:33 +0000

    added forgot and reset password routes

---
+++ utils/sendEmail.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (options) => {
+  console.log(options);
+  const transporter = nodemailer.createTransport({
+    host: "smtp-relay.sendinblue.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: options.to,
+    subject: options.subject,
+    html: options.text,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
